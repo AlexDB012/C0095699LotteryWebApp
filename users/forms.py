19181679f6732e54bedea38c960f0faba1f3b86e
alexb <a.d.bove2@newcastle.ac.uk>
2022-11-07 17:53:15 +0000

    -Added registration form validation

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,34 @@
+import re
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, EmailField, ValidationError
+from wtforms.validators import Email, EqualTo, NoneOf
+
+
+def check_characters(form, field):
+    excluded_characters = "*?!'^+%&/()=}][{$#@<>"
+
+    for character in field.data:
+        if character in excluded_characters:
+            raise ValidationError("The characters '*?!'^+%&/()=}][{$#@<>' are not allowed")
 
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
+
+    def validate_password(self, password):
+        p = re.compile('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,12}$')
+        if not p.match(password.data):
+            raise ValidationError("Password does not meet requirements")
+
+    def validate_phone(self, phone):
+        p = re.compile('\d{4}[-]\d{3}[-]\d{4}')
+        if not p.match(phone.data):
+            raise ValidationError("Phone number must be in form XXXX-XXX-XXXX (including dashes)")
+
+    email = EmailField(validators=[Email()])
+    firstname = StringField(validators=[check_characters])
+    lastname = StringField(validators=[check_characters])
     phone = StringField()
-    password = PasswordField()
-    confirm_password = PasswordField()
+    password = PasswordField(validators=[])
+    confirm_password = PasswordField(validators=[EqualTo('password', message='Both password fields must match')])
     submit = SubmitField()
